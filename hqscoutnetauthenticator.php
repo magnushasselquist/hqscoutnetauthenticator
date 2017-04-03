6ab3f20ab3fc5b4b1a6d08154aeb90c5608bcd2c
Magnus Hasselquist <magnus.hasselquist@gmail.com>
2017-04-03 14:56:19 +0200

    Bättre felhantering
    Hanterar nu skillnaden mellan uppkopplingsfel och inloggningsfel.

--- hqscoutnetauthenticator.php
+++ hqscoutnetauthenticator.php
@@ -57,33 +57,39 @@ class plgAuthenticationhqscoutnetauthenticator extends JPlugin
 			$authUrl = $this->params->get('loginUrl')."?".$this->params->get('usernameParameterName')."=".$credentials['username']."&".$this->params->get('passwordParameterName')."=".$credentials['password'];
 			$authResult = file_get_contents($authUrl);
 
-			if ($authResult <>"") { // Login success
+			if ($authResult <>"") {
 				$authResultObj=json_decode($authResult);	
 				// var_dump($authResultObj); //DEBUG
-				// echo $authResultObj->token;	//DEBUG
-				// echo "Välkommen ".$authResultObj->member->first_name ." ".$authResultObj->member->last_name."!"; //DEBUG
-    			error_log("HQ Login TRUE for: ".$credentials['username'], 0);
-     	 	   	$authMedlemsnr = $authResultObj->member->member_no; 
-     		   	error_log("HQ Medlemsnr FOUND: ".$authMedlemsnr, 0);
-    	    		$query	= $db->getQuery(true) //CMJ: REDOING the query on the correct username, found in Scoutnet.
-  		    		->select('id')
-  		    		->from('#__users')
-  		    		->where('username=' . $db->quote($authMedlemsnr));
-  	        	$db->setQuery($query);
-  	        	$result = $db->loadResult();
-	                $userLoggedIn = JUser::getInstance($result); // Bring this in line with the rest of the system
-		        $response->email = $userLoggedIn->email;
-		        $response->username = $userLoggedIn->username;
-		        $response->type = "hqscoutnetauthenticator";		        
-	    	        $response->status = JAuthentication::STATUS_SUCCESS;
-
-	    	} else { //inloggningen misslyckades
-	       		$response->status = JAuthentication::STATUS_FAILURE;
-	       		$response->error_message = 'Felaktigt användarnamn ('.$credentials['username'].') eller lösenord. Kontrollera uppgifterna och försök igen.';
-  			  	error_log("HQ Login FALSE for ".$username, 0);
-     			// error_log($buffer, 0); //DEBUG
- 		   }
+				if isset($authResultObj->member->member_no) { //login success
+					// echo $authResultObj->token;	//DEBUG
+					// echo "Välkommen ".$authResultObj->member->first_name ." ".$authResultObj->member->last_name."!"; //DEBUG
+					error_log("HQ Login TRUE for: ".$credentials['username'], 0);
+					$authMedlemsnr = $authResultObj->member->member_no; 
+					error_log("HQ Medlemsnr FOUND: ".$authMedlemsnr, 0);
+					$query	= $db->getQuery(true) //CMJ: REDOING the query on the correct username, found in Scoutnet.
+						->select('id')
+						->from('#__users')
+						->where('username=' . $db->quote($authMedlemsnr));
+					$db->setQuery($query);
+					$result = $db->loadResult();
+					$userLoggedIn = JUser::getInstance($result); // Bring this in line with the rest of the system
+					$response->email = $userLoggedIn->email;
+					$response->username = $userLoggedIn->username;
+					$response->type = "hqscoutnetauthenticator";		        
+					$response->status = JAuthentication::STATUS_SUCCESS;
+				} else if isset($authResultObj->err) { //login failure
+					$response->status = JAuthentication::STATUS_FAILURE;
+					$response->error_message = 'Felaktigt användarnamn ('.$credentials['username'].') eller lösenord. Kontrollera uppgifterna och försök igen.';
+						error_log("HQ Login FALSE for ".$username, 0);
+					// error_log($buffer, 0); //DEBUG					
+				}
+			} else { //inloggningen misslyckades
+				$response->status = JAuthentication::STATUS_FAILURE;
+				$response->error_message = 'Uppkopplingen mot Scoutnet misslyckades. Vänligen försök senare.';
+					error_log("HQ Login CONNECTION FAILURE TO SCOUTNET for ".$username, 0);
+				// error_log($buffer, 0); //DEBUG
+			}
 		}
-    }
+	}
 }
 ?>
